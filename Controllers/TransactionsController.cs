506c36323bfae8e21094b77d9166544e222c233d
y1lmaz-Ahmet <ay0201998@gmail.com>
2023-08-18 15:08:19 +0200

    Update operations

--- Controllers/TransactionsController.cs
+++ Controllers/TransactionsController.cs
@@ -26,28 +26,15 @@ namespace BankTransactions.Controllers
                           Problem("Entity set 'TransactionDbContext.Transactions'  is null.");
         }
 
-        // GET: Transactions/Details/5
-        public async Task<IActionResult> Details(int? id)
-        {
-            if (id == null || _context.Transactions == null)
-            {
-                return NotFound();
-            }
-
-            var transaction = await _context.Transactions
-                .FirstOrDefaultAsync(m => m.TransactionId == id);
-            if (transaction == null)
-            {
-                return NotFound();
-            }
-
-            return View(transaction);
-        }
+        
 
 		// GET: Transactions/AddOrEdit
-		public IActionResult AddOrEdit()
+		public IActionResult AddOrEdit(int id = 0)
         {
-            return View(new Transaction());
+            if(id == 0)
+                return View(new Transaction());
+            else
+                return View(_context.Transactions.Find(id));
         }
 
         // POST: Transactions/AddOrEdit
@@ -59,64 +46,21 @@ namespace BankTransactions.Controllers
         {
             if (ModelState.IsValid)
             {
-                transaction.Date = DateTime.Now;
-                _context.Add(transaction);
+                if(transaction.TransactionId == 0)
+                {
+                    transaction.Date = DateTime.Now;
+                    _context.Add(transaction);
+                }
+                else
+                    _context.Update(transaction);
+
                 await _context.SaveChangesAsync();
                 return RedirectToAction(nameof(Index));
             }
             return View(transaction);
         }
 
-        // GET: Transactions/Edit/5
-        public async Task<IActionResult> Edit(int? id)
-        {
-            if (id == null || _context.Transactions == null)
-            {
-                return NotFound();
-            }
-
-            var transaction = await _context.Transactions.FindAsync(id);
-            if (transaction == null)
-            {
-                return NotFound();
-            }
-            return View(transaction);
-        }
-
-        // POST: Transactions/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("TransactionId,AccountNumber,BeneficiaryName,BankName,SWIFTCode,Amount,Date")] Transaction transaction)
-        {
-            if (id != transaction.TransactionId)
-            {
-                return NotFound();
-            }
-
-            if (ModelState.IsValid)
-            {
-                try
-                {
-                    _context.Update(transaction);
-                    await _context.SaveChangesAsync();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!TransactionExists(transaction.TransactionId))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
-                }
-                return RedirectToAction(nameof(Index));
-            }
-            return View(transaction);
-        }
+        
 
         // GET: Transactions/Delete/5
         public async Task<IActionResult> Delete(int? id)
